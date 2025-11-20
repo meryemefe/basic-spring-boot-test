9aa3d14e5d36bc5c3005cb69232b693a8f3dcb9a
Meryem Efe <meryemefe@host10-211-134-237.wlan5.uni-saarland.local>
2025-11-20 11:21:01 +0100

    first commit

---
+++ src/main/java/com/meryemefe/basic_spring_boot_test/BasicSpringBootTestApplication.java
@@ -0,0 +1,13 @@
+package com.meryemefe.basic_spring_boot_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicSpringBootTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicSpringBootTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meryemefe/basic_spring_boot_test/BasicSpringBootTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meryemefe.basic_spring_boot_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicSpringBootTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
